fc2072ff14e148b678d0948189b736fd39b6a167
Sean <sean.o@hotmail.com>
2021-05-22 19:27:15 +0900

    settings adjustment for host

--- iwenthere/settings.py
+++ iwenthere/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '-!p(ek4^p@ad23@(6rjm%s8ixut#(%xrra4cp8p)8&e6@8)l66'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['community.pythonanywhere.com']
 
 # Application definition
 
