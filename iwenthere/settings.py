0e2b82f87fd2f4c23333572a1385e7c0268261da
ohohohobrien <62244420+ohohohobrien@users.noreply.github.com>
2021-05-30 09:23:58 +0900

    Update settings.py

--- iwenthere/settings.py
+++ iwenthere/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-!p(ek4^p@ad23@(6rjm%s8ixut#(%xrra4cp8p)8&e6@8)l66'
+SECRET_KEY = #secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
